e98332331592f7b0695b63f223837ecf5dcb12ba
ductrong <109793528+ductrong0411201@users.noreply.github.com>
2023-05-23 14:49:42 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require("cors");
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-
+const workspaceRouter = require('./routes/workspace');
 const app = express();
 
 // view engine setup
@@ -18,9 +19,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/', usersRouter);
+app.use('/', workspaceRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
